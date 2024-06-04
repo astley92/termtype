d73ee6fe38bdbee0b56543084945b3140b425334
Blake Astley <astley92@hotmail.com>
2024-06-04 22:56:58 +1000

    Print test string by chars and handle some events

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io::{Write, stdout};
-use crossterm::cursor;
+use crossterm::{cursor, event};
 use crossterm::style::{ResetColor, SetForegroundColor};
 use crossterm::terminal;
 use crossterm::style::{Print, Color};
 use crossterm::execute;
 use crossterm::QueueableCommand;
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use std::char;
+
+extern crate fps_clock;
 
 fn main() {
     let mut stdout = stdout();
@@ -14,43 +17,60 @@ fn main() {
 
     const LIGHT_GREY: Color = Color::Rgb { r: 100, g: 100, b: 100 };
     const FPS: u32 = 30;
-    let mut frame_count: u64 = 0;
-    let mut achieved_fps: f64;
-    let mut current_frame_start_time: Instant;
-    let mut current_frame_time_delta: Duration;
-    let required_frame_time_delta: Duration = Duration::from_secs(1) / FPS;
+    let mut fps = fps_clock::FpsClock::new(FPS);
     let test_string_bytes = "Hello, World!".as_bytes();
+
+    let mut frame_count: u64 = 0;
     let start_time = Instant::now();
     let (width, height) = terminal::size().unwrap();
 
     loop {
-        achieved_fps = frame_count as f64 / start_time.elapsed().as_secs_f64();
+        let achieved_fps = frame_count as f64 / start_time.elapsed().as_secs_f64();
         frame_count += 1;
-        current_frame_start_time = Instant::now();
 
-        // Clear the screen and move the cursor to the top left corner
-        stdout.queue(terminal::Clear(terminal::ClearType::All)).unwrap();
+        // UPDATE
+
+        if event::poll(std::time::Duration::from_secs(0)).unwrap() {
+            let event = event::read().unwrap();
+            match event {
+                event::Event::Key(event) => {
+                    if event.code == event::KeyCode::Esc {
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        // DRAW
+        // Clear the screen
+        stdout
+            .queue(terminal::Clear(terminal::ClearType::All)).unwrap()
+            .queue(ResetColor).unwrap();
 
         // Print the test string
         stdout
             .queue(SetForegroundColor(LIGHT_GREY)).unwrap()
-            .queue(cursor::MoveTo(0, 0)).unwrap()
-            .queue(Print(std::str::from_utf8(test_string_bytes).unwrap())).unwrap()
-            .queue(ResetColor).unwrap();
+            .queue(cursor::MoveTo(0, 1)).unwrap();
+
+        let mut str_index = 0;
+        while str_index < test_string_bytes.len() as u32 {
+            let ch = char::from_u32(test_string_bytes[str_index as usize] as u32).unwrap();
+            stdout.queue(Print(ch)).unwrap();
+            str_index += 1;
+        }
 
         // Print debugging information
         stdout
             .queue(cursor::MoveTo(width - 5, height - 1)).unwrap()
-            .queue(Print(format!("{:.2}", achieved_fps))).unwrap();
+            .queue(Print(format!("{:.2}", achieved_fps))).unwrap()
+            .queue(ResetColor).unwrap()
+            .queue(cursor::MoveTo(0, 0)).unwrap()
+            .queue(Print(format!("{:.2}", start_time.elapsed().as_secs()))).unwrap();
 
         stdout.flush().unwrap();
 
-        if frame_count > 100 {
-            break;
-        }
-
-        current_frame_time_delta = Instant::now().duration_since(current_frame_start_time);
-        std::thread::sleep(required_frame_time_delta - current_frame_time_delta);
+        fps.tick();
     }
 
     execute!(stdout, cursor::Show).unwrap();
